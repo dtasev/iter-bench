359086936d7e6d225f2c83d8de814aa858886dbb
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-04-17 13:53:30 +0100

    flower_pp uses Flat_Before, changed recon_flower to pp_flower

---
+++ pp_flower.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+file=times-$(hostname)-recon.txt
+echo "" > $file
+output=out-$(hostname)-recon.txt
+echo "" > $output
+TIME="/usr/bin/time -ao $file -v"
+set -x
+$TIME ./savu_mpi_local.sh /home/dt1107582/flower/ flower_pp.nxs out >> $output 2>&1
+# savu out
+
+set +x
+
+
+
