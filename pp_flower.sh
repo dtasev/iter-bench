22a9634c6f8ac1eebef6ce502b429bab0848c322
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-04-17 16:10:45 +0100

    Recon times for ndw1893 and script updates

--- pp_flower.sh
+++ pp_flower.sh
@@ -6,7 +6,7 @@ output=out-$(hostname)-pp.txt
 echo "" > $output
 TIME="/usr/bin/time -ao $file -v"
 set -x
-$TIME ./savu_mpi_local.sh /home/dt1107582/flower/ flower_pp.nxs out >> $output 2>&1
+$TIME ./savu_mpi_local.sh $1 flower_pp.nxs out >> $output 2>&1
 # savu out
 
 set +x
--- recon_flower.sh
+++ recon_flower.sh
@@ -6,7 +6,7 @@ output=out-$(hostname)-recon.txt
 echo "" > $output
 TIME="/usr/bin/time -ao $file -v"
 set -x
-$TIME savu out/2*/Flower_WhiteBeam_processed.nxs flower_recon.nxs out >> $output 2>&1
+$TIME savu out/2*/flower_processed.nxs flower_recon.nxs out >> $output 2>&1
 # savu out
 
 set +x
