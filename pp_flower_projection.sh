f1e9af85f641cd84cbc9f999570c3642b16a0262
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-04-22 10:11:22 +0100

    Added flower projection pre-processing run

---
+++ pp_flower_projection.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+file=times-$(hostname)-pp.txt
+echo "" > $file
+output=out-$(hostname)-pp.txt
+echo "" > $output
+TIME="/usr/bin/time -ao $file -v"
+set -x
+$TIME ./savu_mpi_local.sh $1 flower_pp_projection.nxs out >> $output 2>&1
+
+set +x
+
+
+
